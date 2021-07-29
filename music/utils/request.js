9143482abfdfe574414e399b4fd5bb42635e3d6b
zhuxx <xiaoxiangzhu1996@gmail.com>
2021-07-29 18:31:49 +0800

    third commit

---
+++ music/utils/request.js
@@ -0,0 +1,33 @@
+// 发送ajax请求
+/*
+1封装功能函数
+  1功能点明确
+  2函数内部应该保留固定代码（静态的）
+  3将动态的数据抽取成形参，由使用者根据自身的情况动态的传入实参
+  4一个良好的功能函数应该设置形参的默认值（es6）
+2封装功能组件
+  1功能点明确
+  2组件的内部保留静态的代码
+  3将动态的数据抽取成props参数，由使用者根据自身的情况以标签属性的形式动态传入props数据
+  4一个良好的组件应该设置组件的必要性及数据类型
+
+*/
+import config from './config'
+export default (url, data={}, method='GET') => {
+  return new Promise((resolve, reject) => {
+    //1 new Promise 初始化promise实例的状态为pending
+    wx.request({
+      url: config.host + url,
+      data,
+      method,
+      success: (res) =>{
+        console.log("请求成功",res);
+        resolve(res.data);
+      },
+      fail: (err) =>{
+        console.log("shibai",err);
+        reject(err)
+      }
+    })
+  })
+}
--- wechat_study/pages/indeix/index.js
+++ wechat_study/pages/indeix/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    msg: '初始化测试数据'
+    userInfo: {},
+    msg: '初始化测试数据',
   },
   handleParent(){
     console.log('parent')
@@ -13,43 +14,83 @@ Page({
   handleChild(){
     console.log('child')
   },
+  // 跳转至logs页面的方法
+  toLogs(){
+    wx.navigateTo({
+      url: '/pages/logs/logs',
+    })
+  },
+  // toLogs(){
+  //   wx.redirectTo({
+  //     url: '/pages/logs/logs',
+  //   })
+  // },
+  // toLogs(){
+  //   wx.reLaunch({
+  //     url: '/pages/logs/logs',
+  //   })
+  // },
+  getUserProfile(){
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        let user = res.userInfo
+        wx.setStorageSync('user', user)
+        this.setData({
+          userInfo: user
+        })
+        console.log(res);
+      },
+    })
+  },
+  logout(){
+    this.setData({
+      userInfo: {}
+    })
+    wx.setStorageSync('user', null)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(this.data.msg)
+    // console.log(this.data.msg)
+    // this.setData({
+    //   msg: '修改后的数据'
+    // })
+    // console.log(this.data.msg)
+    let user = wx.getStorageSync('user')
     this.setData({
-      msg: '修改后的数据'
+      userInfo: user
     })
-    console.log(this.data.msg)
+    console.log('onLoad')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('onReady')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('onUnload')
   },
 
   /**
